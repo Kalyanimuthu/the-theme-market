4d5224745a615df85c50cea693a6c6a3b89b7dd2
Kalyanimuthu <kalyaniram@gmail.com>
2025-11-18 03:03:40 +0530

    updated settings.py

--- thememarket/settings.py
+++ thememarket/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-*x9=@k#3l*2s6!civa-o+=oy4fylkzoxq&*l$-ps_u%$ezwt7-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['the-theme-market.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
